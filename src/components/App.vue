49212c568a759df32137a568bf62285c5006b8d1
Eric McCormick <edm00se@gmail.com>
2018-11-02 20:56:50 -0500

    chore(format): adds husky and lint-staged
    adds prettier formatting to pre-commit and pre-push

--- src/components/App.vue
+++ src/components/App.vue
@@ -16,42 +16,41 @@ export default {
       subtitle: 'from <a href="https://vuejs.org/">vue.js</a>!'
     };
   }
-}
+};
 </script>
 
 <style lang="scss">
-  html,
-  body {
-    height: 100%;
-  }
+html,
+body {
+  height: 100%;
+}
 
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial,
+body {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial,
     sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-  }
+}
 
-  .wrapper {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-  }
+.wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
 
-  .content {
-    text-align: center;
-  }
+.content {
+  text-align: center;
+}
 
-  h1 {
-    font-weight: 300;
-  }
+h1 {
+  font-weight: 300;
+}
 
-  h2 {
-    font-weight: 500;
-  }
+h2 {
+  font-weight: 500;
+}
 
-  a {
-    color: #42b983;
-  }
+a {
+  color: #42b983;
+}
 </style>
-
