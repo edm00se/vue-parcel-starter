af3b2f41238aba3bdd084e2302cf89503fef6544
Eric McCormick <edm00se@gmail.com>
2021-10-13 12:39:57 -0500

    config clean up

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  setupFiles: ['<rootDir>/config/tests/setup'],
   coverageDirectory: '<rootDir>/config/tests/coverage',
   moduleFileExtensions: [ 'js', 'json', 'vue' ],
   moduleNameMapper: {
